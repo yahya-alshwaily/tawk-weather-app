16ff4d28cbc4272491e0f2ce5099094da1aac017
Yahya Alshwaily <yahya_alshwaily@yahoo.com>
2025-03-31 23:44:04 +0300

    chore(types): add return data types to functions

---
+++ src/types/weather.ts
@@ -0,0 +1,55 @@
+import { ref } from "vue";
+
+export interface HourData {
+  time: string;
+  temp: number;
+  icon: string;
+}
+
+export interface DayData {
+  day: string;
+  temp: number;
+  condition: string;
+  icon: string;
+}
+
+export interface ForecastResponse {
+  list: ForecastEntry[]
+  city: {
+    name: string
+    country: string
+    timezone: number
+  }
+}
+
+export interface ForecastEntry {
+  dt: number;
+  main: {
+    temp: number;
+    temp_min: number;
+    temp_max: number;
+  };
+  weather: {
+    main: string;
+    description: string;
+    icon: string;
+  }[];
+  dt_txt: string;
+}
+
+export interface CurrentWeatherResponse {
+    name: string
+    sys: {
+      country: string
+    }
+    main: {
+      temp: number
+      temp_min: number
+      temp_max: number
+    }
+    weather: {
+      description: string
+      icon: string
+    }[]
+  }
+  
