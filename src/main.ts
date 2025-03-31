ace14d1934866735e80547d26d3334cbc88ad334
Yahya Alshwaily <yahya_alshwaily@yahoo.com>
2025-03-31 19:18:30 +0300

    feat(components,plugins): add base Icon component and integrate FontAwesome icons plugin

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 import { createApp } from 'vue'
-import './style.css'
 import App from './App.vue'
 import router from './router'
 
+import { FontAwesomeIcon } from './plugins/fontawesome'
+
 const app = createApp(App)
+app.component('FontAwesomeIcon', FontAwesomeIcon)
 app.use(router)
 app.mount('#app')
---
+++ src/plugins/fontawesome.ts
@@ -0,0 +1,14 @@
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+import {
+  faSearch,
+  faLocationDot,
+  faCloud,
+  faSun,
+  faChevronRight,
+} from '@fortawesome/free-solid-svg-icons'
+
+library.add(faSearch, faLocationDot, faCloud, faSun, faChevronRight)
+
+export { FontAwesomeIcon }
