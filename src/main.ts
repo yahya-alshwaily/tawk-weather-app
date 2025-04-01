99593809c6ae92bba4bf683ed677c85b983c12ad
Yahya Alshwaily <yahya_alshwaily@yahoo.com>
2025-04-01 10:10:59 +0300

    feat(utils): install lodash and use it in main setup

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,8 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-
 import { FontAwesomeIcon } from './plugins/fontawesome'
+import '@/assets/style.css'
 
 const app = createApp(App)
 app.component('FontAwesomeIcon', FontAwesomeIcon)
